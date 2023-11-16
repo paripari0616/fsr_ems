7c683f3e36631c2e747f815f953b1e8acb1bcaa5
wu <lzx9502@gmail.com>
2023-11-16 12:03:58 +0900

    Revert "wu"
    This reverts commit d3fb7073aeb1d79044db174883a2816ff3956d26.

--- src/main/resources/db/migrations/data.sql
+++ src/main/resources/db/migrations/data.sql
@@ -7,3 +7,7 @@ INSERT INTO public.m_code (id, type, code, name) VALUES
 -- t_user データ
 INSERT INTO public.t_user (id, username, realname, password, gender) VALUES
 (8, '123', '123', '123', '0');
+
+-- t_emp テーブル
+INSERT INTO public.PARTY_B (COMPANY_ID, ADDRESS, TEL, FAX) VALUES
+(1, 'ＦＳＲ株式会社', '〒110-000 東京都台東区上野3-19-4 KEN BUILDING 5階', '03-6876-0986', '03-6876-0986 ');
