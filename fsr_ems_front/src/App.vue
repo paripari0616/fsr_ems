07fb95815aa97a912b7b4da19ab78b3e72a521ab
dada <lzw27027879@gmail.com>
2023-11-20 17:40:31 +0900

    フロントエンドの初期化

---
+++ fsr_ems_front/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- ルートアウトレット -->
+    <!-- ルートとマッチしたコンポーネントがここへ描画されます -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="less">
+html,
+body,
+h3,
+p {
+  margin: 0;
+  padding: 0;
+}
+</style>
